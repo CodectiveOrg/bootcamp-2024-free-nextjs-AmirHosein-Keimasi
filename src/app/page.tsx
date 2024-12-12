fc3c76bbcb3d65451d1789d447d9e1de4fd1b3df
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2024-12-12 15:56:35 +0330

    [Update] page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-<h1>سلام رفیق!</h1>
+    <main>
+      <h1>سلام رفیق!</h1>
     </main>
   );
 }
