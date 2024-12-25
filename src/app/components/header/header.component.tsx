dc90ca3a6bbf8e362e3f287e3eb36d1f48e888ed
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2024-12-25 15:31:26 +0330

    [ADD] header [ADD] footer

---
+++ src/app/components/header/header.component.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ReactElement } from "react";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import clsx from "clsx";
+
+import styles from "./header.module.css";
+
+const links = [
+  { href: "/", title: "خانه" },
+  { href: "/search", title: "جستجو" },
+];
+
+export default function HeaderComponent(): ReactElement {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={clsx(pathname === link.href && styles.active)}
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button className={styles.cta}>ورود | ثبت‌نام</button>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
+import { ReactElement } from "react";
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
-import "./globals.css";
 
-const vazirmatn = Vazirmatn({ subsets: ["latin", "arabic"], display: "swap" });
+import HeaderComponent from "./components/header/header.component";
+
+import "./globals.css";
+import FooterComponent from "./components/footer/footer.component";
+
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "رزور نوبت",
@@ -13,10 +21,17 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
-    <html lang="fa" dir="rtl">
-      <body className={vazirmatn.className}>{children}</body>{" "}
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          پلتفرمی یکپارچه برای یافتن پزشک و رزرو آنلاین نوبت{" "}
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
