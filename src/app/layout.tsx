559aa0a722cd0f6527a24b90543b6fb56076ba95
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2024-12-26 00:09:11 +0330

    Lesson 2 (#6)
    * [UPDATE] README.md
    [UPDATE] .gitignore
    
    * [PRETTIER]
    
    * [ADD] lint.yml
    
    * [ADD] postcss
    
    * [Add] Css reset
    
    * [ADD] favicon
    
    * [ADD] header
    [ADD] footer
    
    * [UPDATE] Folders
    [UPDATE] home page
    
    * [UPDATE] Apply Prettier formatting to the file
    
    * [UPDATE] ESLint
    
    * [UPDATE] eslintrc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
+import { ReactElement } from "react";
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
 import "./globals.css";
 
-const vazirmatn = Vazirmatn({ subsets: ["latin", "arabic"], display: "swap" });
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "رزور نوبت",
@@ -13,10 +21,17 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
-    <html lang="fa" dir="rtl">
-      <body className={vazirmatn.className}>{children}</body>{" "}
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          پلتفرمی یکپارچه برای یافتن پزشک و رزرو آنلاین نوبت{" "}
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { ReactElement } from "react";
+
+import MyDoctorLogo from "@/logo/my-doctor.logo";
+
+import styles from "./page.module.css";
+import GlobalSearchBoxComponent from "@/components/globall-search-box/globall-search-box.component";
+
+export default function Home(): ReactElement {
   return (
-    <main>
-      <h1>سلام رفیق!</h1>
-    </main>
+    <div className={styles.home}>
+      <h1>
+        <MyDoctorLogo />
+        دکتر من
+      </h1>
+      <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>ارتوپد</li>
+          <li>قلب و عروق</li>
+        </ul>
+      </div>
+    </div>
   );
 }
