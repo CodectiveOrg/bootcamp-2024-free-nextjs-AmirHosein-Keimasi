f3a1a1f320ec27b468a5f1d9d77c5d17afec2c05
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-01-06 11:25:05 +0330

    [ADD] db.doctors [ADD] Types of doctors

---
+++ src/models/doctor.model.tsx
@@ -0,0 +1,14 @@
+export interface DoctorModel {
+  id: string;
+  name: string;
+  image: string;
+  isVerified: boolean;
+  averageRating: number;
+  totalVotes: number;
+  address: string;
+  firstAvailableAppointment: string;
+  brief: string;
+  badges: string[];
+  gender: string;
+  appointmentTypes: string[];
+}
