c4e12f89422f8ebdbd9682ed13620a5e8027776f
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-03-22 16:33:25 +0330

    [ADD] filter components expertise Gender

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,6 @@
 import { ReactElement } from "react";
 
-
 import GlobalSearchBoxComponent from "@/components/globall-search-box/globall-search-box.component";
-
 
 import { FiltersType } from "@/types/filter.type";
 import { doctors } from "@/db/doctors";
@@ -21,8 +19,11 @@ import DoctorsProvider from "./providers/doctors.provider";
 
 import styles from "./page.module.css";
 import ResultsComponent from "./components/results/results.component";
-
-
+import FiltersSummaryComponent from "./components/filters-summary/filters-summary.component";
+import DegreeFilterComponent from "./components/degree-filter/degree-filter.component";
+import AppointmentFilterComponent from "./components/appointment-filter/appointment-filter.component";
+import ExpertiseFilterComponent from "./components/expertise-filter/expertise-filter.component";
+import GenderFilterComponent from "./components/gender-filter/gender-filter.component";
 
 type SearchParams = { [key: string]: string | string[] | undefined };
 
@@ -43,20 +44,18 @@ export default async function Page({
             <GlobalSearchBoxComponent />
           </div>
           <div className={styles.filters}>
-            {/* <FiltersSummaryComponent />
+            <FiltersSummaryComponent />
             <ExpertiseFilterComponent />
             <GenderFilterComponent />
-            <DegreeFilterComponent /> */}
+            <DegreeFilterComponent />
           </div>
           <div className={styles.toolbar}>
-            {/* <SortComponent />
-            <AppointmentFilterComponent /> */}
-            <div className={styles.stats}>
-              {/* <StatsComponent /> */}
-            </div>
+            {/* <SortComponent /> */}
+            <AppointmentFilterComponent />
+            <div className={styles.stats}>{/* <StatsComponent /> */}</div>
           </div>
           <div className={styles.results}>
-            <ResultsComponent /> 
+            <ResultsComponent />
           </div>
         </div>
       </DoctorsProvider>
