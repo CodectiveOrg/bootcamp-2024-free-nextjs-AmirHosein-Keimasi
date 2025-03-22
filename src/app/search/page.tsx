d7601e5e950ba9e7b1b0a7ca051c3ce85ac9bcc5
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-03-22 15:55:51 +0330

    [ADD] result page [ADD] Search Page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,28 @@
 import { ReactElement } from "react";
 
+
+import GlobalSearchBoxComponent from "@/components/globall-search-box/globall-search-box.component";
+
+
 import { FiltersType } from "@/types/filter.type";
 import { doctors } from "@/db/doctors";
 
-import Doctorcard from "@/components/doctor-card/doctorcard.component";
+// import AppointmentFilterComponent from "@/app/search/components/appointment-filter/appointment-filter.component";
+// import DegreeFilterComponent from "@/app/search/components/degree-filter/degree-filter.component";
+// import ExpertiseFilterComponent from "@/app/search/components/expertise-filter/expertise-filter.component";
+// import FiltersSummaryComponent from "@/app/search/components/filters-summary/filters-summary.component";
+// import GenderFilterComponent from "@/app/search/components/gender-filter/gender-filter.component";
+// import ResultsComponent from "@/app/search/components/results/results.component";
+// import SortComponent from "@/app/search/components/sort/sort.component";
+// import StatsComponent from "@/app/search/components/stats/stats.component";
 
-import MingcuteFilter3Fill from "@/icons/MingcuteFilter3Fill";
+import FiltersProvider from "./providers/filtersProviders";
+import DoctorsProvider from "./providers/doctors.provider";
 
 import styles from "./page.module.css";
-import FiltersProvider from "./providers/filtersProviders";
+import ResultsComponent from "./components/results/results.component";
+
+
 
 type SearchParams = { [key: string]: string | string[] | undefined };
 
@@ -23,36 +37,50 @@ export default async function Page({
 
   return (
     <FiltersProvider defaultFilters={defaultFilters}>
-      <div className={styles.header}>
-        <MingcuteFilter3Fill />
-
-        <h3>فیلتر ها</h3>
-      </div>
-      <div className={styles.page}>
-        <div className=""></div>
-        <Doctorcard doctors={doctors} />
-      </div>
+      <DoctorsProvider doctors={doctors}>
+        <div className={styles.page}>
+          <div className={styles.search}>
+            <GlobalSearchBoxComponent />
+          </div>
+          <div className={styles.filters}>
+            {/* <FiltersSummaryComponent />
+            <ExpertiseFilterComponent />
+            <GenderFilterComponent />
+            <DegreeFilterComponent /> */}
+          </div>
+          <div className={styles.toolbar}>
+            {/* <SortComponent />
+            <AppointmentFilterComponent /> */}
+            <div className={styles.stats}>
+              {/* <StatsComponent /> */}
+            </div>
+          </div>
+          <div className={styles.results}>
+            <ResultsComponent /> 
+          </div>
+        </div>
+      </DoctorsProvider>
     </FiltersProvider>
   );
+}
 
-  function generateDefaultFilters(searchParams: SearchParams): FiltersType {
-    const { query, expertise, gender, degree } = searchParams;
+function generateDefaultFilters(searchParams: SearchParams): FiltersType {
+  const { query, expertise, gender, degree } = searchParams;
 
-    return {
-      query: normalizeFilter(query),
-      expertise: normalizeFilter(expertise),
-      gender: normalizeFilter(gender),
-      degree: normalizeFilter(degree),
-    };
+  return {
+    query: normalizeFilter(query),
+    expertise: normalizeFilter(expertise),
+    gender: normalizeFilter(gender),
+    degree: normalizeFilter(degree),
+  };
+}
+
+function normalizeFilter(
+  value: string | string[] | undefined,
+): string | undefined {
+  if (Array.isArray(value)) {
+    return value[0];
   }
 
-  function normalizeFilter(
-    value: string | string[] | undefined,
-  ): string | undefined {
-    if (Array.isArray(value)) {
-      return value[0];
-    }
-
-    return value;
-  }
+  return value;
 }
