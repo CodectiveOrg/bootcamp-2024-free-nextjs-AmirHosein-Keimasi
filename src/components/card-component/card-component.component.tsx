99a9eb3025a03546713475b551b684d88549df85
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-03-23 19:24:47 +0330

    [DELETE] Old components from lesson-4

--- src/components/card-component/card-component.component.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren, ReactElement } from "react";
-
-import styles from "./card-component.module.css";
-
-type Props = PropsWithChildren;
-
-export default function CardComponent({ children }: Props): ReactElement {
-  return <div className={styles.card}>{children}</div>;
-}
